4e92fa5bd5fa4b723c81c47a411784874575aa3a
Hestys <hestu12@gmail.com>
2020-12-11 00:27:47 +0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -9,105 +13,98 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Survey',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SurveyList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SurveyList extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _SurveyListState createState() => _SurveyListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _SurveyListState extends State<SurveyList> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("Simple Survey"),
+          centerTitle: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: StreamBuilder<QuerySnapshot>(
+          stream:
+              FirebaseFirestore.instance.collection("dilanketi").snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(child: LinearProgressIndicator());
+            } else {
+             return buildBody(context, snapshot.data.docs);
+            }
+          },
+        )
+        //
+        );
+  }
+
+  buildBody(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return ListView(
+      padding: EdgeInsets.only(top: 20),
+      children:
+          snapshot.map<Widget>((data) => buildListItem(context, data)).toList(),
     );
   }
+
+  buildListItem(BuildContext context, DocumentSnapshot data) {
+    final row = Survey.fromSnapshot(data);
+    return Padding(
+      key: ValueKey(row.isim),
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(5.0)),
+        child: ListTile(
+          title: Text(row.isim),
+          trailing: Text(row.oy.toString()),
+          onTap: () {
+            FirebaseFirestore.instance.runTransaction((transaction) async{
+              final freshSnapshot = await transaction.get(row.reference);//Snapshot
+              final fresh = Survey.fromSnapshot(freshSnapshot);//Survey
+              
+              await transaction.update(row.reference, {"oy":fresh.oy+1});
+
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
+
+final fakeSnapshot = [
+  {"isim": "C#", "oy": 3},
+  {"isim": "Java", "oy": 6},
+  {"isim": "C++", "oy": 12},
+  {"isim": "Pyhthon", "oy": 44}
+];
+
+class Survey {
+  String isim;
+  int oy;
+
+  DocumentReference reference;
+
+  Survey.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map["isim"] != null),
+        assert(map["oy"] != null),
+        isim = map["isim"],
+        oy = map["oy"];
+
+  Survey.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data(), reference: snapshot.reference);
 }
